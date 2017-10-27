2b069103767ad3faa4848453f98837deb6b32c39
theimberger <theimberger@gmail.com>
2017-10-27 10:04:30 -0400

    Add error handling to spaces add form

--- frontend/components/welcome_page/spaces_index/add_space.jsx
+++ frontend/components/welcome_page/spaces_index/add_space.jsx
@@ -4,18 +4,83 @@ class AddSpace extends React.Component {
 
   constructor(props) {
     super();
+    this.state = {
+      step: 1,
+      title: "",
+      description: "",
+      warning: ""
+    };
 
+    this.changeStep.bind(this);
+    this.update.bind(this);
   }
 
+  changeStep(e) {
+    e.preventDefault();
+    let prev = this.state;
+    if (this.state.step === 1 & this.state.title === "") {
+      document.getElementsByTagName('input')[0].focus();
+      prev.warning = "Please enter a space name.";
+    } else {
+      prev.step += 1;
+    }
+    this.setState( prev );
+  }
+
+  update(e) {
+    let newState = this.state;
+    newState.warning = "";
+    if (this.state.step === 1){
+      newState.title = e.currentTarget.value;
+    } else {
+      newState.description = e.currentTarget.value;
+    }
+    this.setState(newState);
+  }
 
   render() {
+    let labelText;
+    let input;
+    let buttonText;
+    let errors;
+
+    if (this.state.step === 1){
+      labelText = "What's your space called?";
+
+      input = <input
+        placeholder="ex. my cool space"
+        onChange={(e) => this.update(e)}>
+        </input>;
+
+      if (this.state.title ==="") {
+        input = <input value={this.state.title}
+          onChange={(e) => this.update(e)}>
+          </input>;
+      }
+      buttonText = "Next";
+    } else {
+      labelText = "Give your space a description (optional).";
+      input = <textarea defaultValue={this.state.description}
+        onChange={(e) => this.update(e)}>
+        </textarea>;
+      buttonText = "Submit";
+      if (this.state.description === "") {
+        buttonText = "No, just submit";
+      }
+    }
+
+    if (this.state.errors !== "") {
+      errors = <span className="errors" style={{fontSize: "2em"}}><br/>{this.state.warning}</span>;
+    }
 
     return (
       <div className="add_space">
         <Nav />
-        <form>
-          <label>What's Your New Space Called?</label>
-          <input placeholder="ex: my cool space"></input>
+        <form onSubmit={ (e) => this.changeStep(e) }>
+          <label>{labelText}</label>
+          {input}
+          {errors}
+          <button>{buttonText}</button>
         </form>
       </div>
     );
