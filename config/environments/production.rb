e6f9886c1b19093a9c50e87137f28fb3d9187e4d
theimberger <theimberger@gmail.com>
2017-11-03 10:12:04 -0400

    Update config to allow redis on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.web_socket_server_url = "wss://slack-clone-relax.herokuapp.com/cable"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -42,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['https://slack-clone-relax.herokuapp.com', 'http://slack-clone-relax.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
