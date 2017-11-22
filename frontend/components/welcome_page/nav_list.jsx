b5c3a07a424a1f212f7f4263e7d666e7ceb01264
theimberger <theimberger@gmail.com>
2017-11-22 09:07:59 -0500

    Add links to welcome page

--- frontend/components/welcome_page/nav_list.jsx
+++ frontend/components/welcome_page/nav_list.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 
 const NavList = () => (
   <ul>
-    <li>Github /</li>
-    <li>Relax Repo /</li>
-    <li>LinkedIn /</li>
-    <li>Angel List /</li>
-    <li><button>//t</button></li>
+    <li>
+      <a href="https://github.com/theimberger">Github</a> /
+    </li>
+    <li>
+      <a href="https://github.com/theimberger/relax">Relax Repo</a> /
+    </li>
+    <li>
+      <a href="https://www.linkedin.com/in/thomas-heimberger-46b255141/">LinkedIn</a> /
+    </li>
+    <li>
+      <a href="https://angel.co/thomas-heimberger">Angel List</a>
+    </li>
+    <li><a href="http://thomasheimberger.com">
+        <button>// t</button>
+    </a></li>
   </ul>
 );
 
